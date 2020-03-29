c9faedbb536449275cbd378a9a548dadbb36dce8
com <com@com-PC>
2020-03-29 22:19:20 +0530

    Fix Cross Domain Issue for OAuth request

--- spring-jdbc-role-security-poc/src/main/java/com/logical/jdbc/demo/config/WebSecurityConfig.java
+++ spring-jdbc-role-security-poc/src/main/java/com/logical/jdbc/demo/config/WebSecurityConfig.java
@@ -3,49 +3,70 @@ package com.logical.jdbc.demo.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Order(2)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private DataSource dataSource;
-  
+	@Autowired
+	private DataSource dataSource;
 
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-  
- @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    auth.jdbcAuthentication().dataSource(dataSource)
-        .usersByUsernameQuery("select username, password, enabled"
-            + " from users where username=?")
-        .authoritiesByUsernameQuery("select username, authority "
-            + "from authorities where username=?")
-        .passwordEncoder(bCryptPasswordEncoder());
-  }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception { 
-	  http.csrf().disable().authorizeRequests().and().authorizeRequests().antMatchers("/oauth/token")
-      .permitAll().anyRequest().authenticated();
-	  }
-  
-  @Override
-  @Bean
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
-  
- }
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled" + " from users where username=?")
+				.authoritiesByUsernameQuery("select username, authority " + "from authorities where username=?")
+				.passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/oauth/token").permitAll().and()
+				.antMatcher("/**").authorizeRequests().and().authorizeRequests().anyRequest().authenticated();		
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Bean
+	public FilterRegistrationBean corsFilter1() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		@SuppressWarnings("unchecked")
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+}
--- spring-jdbc-role-security-poc/src/main/java/com/logical/jdbc/demo/controller/EmployeeController.java
+++ spring-jdbc-role-security-poc/src/main/java/com/logical/jdbc/demo/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.logical.jdbc.demo.model.Employees;
 import com.logical.jdbc.demo.service.EmployeeService;
 
 @RestController
+@CrossOrigin
 public class EmployeeController {
 
 	@Autowired
@@ -24,5 +26,5 @@ public class EmployeeController {
 		return employeeService.getAllEmployees();
 
 	}
-
+	
 }
