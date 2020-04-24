aa229d3f24c7514f3096f856c90a47aa05768ae2
Bhushan D <bhushan.a.deshpande@capgemini.com>
2020-04-24 13:35:36 +0530

    handling multiple result set for same column SP

--- spring-jdbc-role-security-poc/src/main/java/com/logical/jdbc/demo/repository/EmployeeRepositoryImpl.java
+++ spring-jdbc-role-security-poc/src/main/java/com/logical/jdbc/demo/repository/EmployeeRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.logical.jdbc.demo.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,13 +24,31 @@ public class EmployeeRepositoryImpl {
 	@PersistenceContext
 	private EntityManager em;
 
-	
-
 	public List<Employees> getAllEmployees() {
+		List<Employees> emp = new ArrayList<Employees>();
 		StoredProcedureQuery findByYearProcedure = em.createStoredProcedureQuery("get_All_Employees");
+		List<List<Object[]>> finalresult = new ArrayList<List<Object[]>>();
 		findByYearProcedure.execute();
 		List<Object[]> result = findByYearProcedure.getResultList();
-		return result.stream().map(employee -> new Employees((int) employee[0], (String) employee[1],
-				(String) employee[2], (String) employee[3])).collect(Collectors.toList());
+		finalresult.add(result);
+		if (findByYearProcedure.hasMoreResults()) {
+			System.out.println("yes it has ");
+			List<Object[]> result2 = findByYearProcedure.getResultList();
+			finalresult.add(result2);
+			System.out.println(result2.toString());
+		}
+		for (List<Object[]> out : finalresult) {
+			for (Object[] out1 : out) {
+				emp.add(new Employees((int) out1[0], (String) out1[1], (String) out1[2], (String) out1[3]));
+			}
+		}
+		return emp;
 	}
 }
+// SP code changes , please update SP for multiple result set to below one and add record into Emplyee table
+/*CREATE DEFINER=`root`@`localhost` PROCEDURE `get_all_employees`()
+BEGIN
+select * from employees where employeeID = 1 ;
+select * from employees where employeeID = 2 ;
+END*/
+//INSERT INTO `demo_database`.`employees` (`employeeID`, `firstName`, `lastName`, `address`) VALUES ('2', 'test', 'test1', 'test 3');
